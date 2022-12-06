73b4eef80743835e6bc88d29bb026700dcaf82fd
Constance Lelievre <constance.lelievre@gmail.com>
2022-12-06 17:40:09 +0100

    articles links and Q/A

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,4 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  get 'answers/index'
-  get 'answers/show'
-  get 'questions/index'
-  get 'questions/show'
-  get 'articles/index'
-  get 'articles/show'
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
