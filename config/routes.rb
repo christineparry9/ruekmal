bf5ccc3f693efe7dd3abd6d1eb3b0351d506f813
Christine Parry <christine.parry9@gmail.com>
2022-12-06 17:10:11 +0000

    back end

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  get 'answers/index'
-  get 'answers/show'
-  get 'questions/index'
-  get 'questions/show'
-  get 'articles/index'
-  get 'articles/show'
+
   devise_for :users
   root to: "pages#home"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
