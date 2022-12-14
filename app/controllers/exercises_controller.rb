e6eeb54d7bad2c1e77eb58a1f8242ec018abe5b5
Christine Parry <christine.parry9@gmail.com>
2022-12-14 12:49:45 +0100

    for you category for exercises, layout issues, background colour

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,9 +1,11 @@
 class ExercisesController < ApplicationController
 
   def index
+    @user = current_user
     @exercises = Exercise.all
     @lower = Exercise.where(category: 'lower')
     @neck = Exercise.where(category: 'neck')
     @upper = Exercise.where(category: 'upper')
+    @foryou = @user.program.exercises.all
   end
 end
